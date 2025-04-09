2f5b0b77eaf666df04805d422317601b01717e15
Griga <griga.k.r.21@gmail.com>
2025-04-09 17:56:45 +0300

    add all buttons

--- deleteVCH.py
+++ deleteVCH.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import cv2
@@ -10,6 +11,7 @@ from PyQt5.QtGui import QPixmap, QImage
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from PIL import Image
+
 
 class ImageGraphApp(QWidget):
     def __init__(self):
@@ -48,7 +50,7 @@ class ImageGraphApp(QWidget):
         # Создаём изображения numpy (прямо в коде)
         self.image_data = [
             np.full((1200, 1300, 3), [255, 0, 0], dtype=np.uint8),  # Красное
-            np.full((1200, 1300, 3), [0, 0, 255], dtype=np.uint8)   # Синее
+            np.full((1200, 1300, 3), [0, 0, 255], dtype=np.uint8)  # Синее
         ]
 
         # Виджет для изображения
@@ -71,20 +73,19 @@ class ImageGraphApp(QWidget):
         self.slider.setFocusPolicy(
             Qt.StrongFocus
         )  # Ensure the slider can take keyboard focus
-
+        self.setFocus()
 
         # Кнопка
         self.next_button = QPushButton("Next")
-        #self.next_button.clicked.connect(self.slider_changed)
+        self.next_button.clicked.connect(self.slider_changed_by_button)
 
         self.back_button = QPushButton("Back")
-
 
         self.open_video_button = QPushButton("Open Video")
         self.open_video_button.clicked.connect(self.open_video)
 
         self.open_csv_button = QPushButton("Open CSV")
-
+        self.open_csv_button.clicked.connect(self.read_data("./","SCI_3_dpi_1_angles.csv"))
 
         # Layout
         h_layout = QHBoxLayout()
@@ -104,16 +105,14 @@ class ImageGraphApp(QWidget):
 
         v_layout = QVBoxLayout()
         v_layout.addLayout(h_layout)
-        v_layout.addLayout( navigation_layout)
+        v_layout.addLayout(navigation_layout)
         v_layout.addLayout(buttons_layout)
         v_layout.addLayout(buttons_video_layout)
-
-
 
         self.setLayout(v_layout)
 
         # Первый вызов
-        self.update_content()
+        # self.update_content()
 
     def update_content(self):
         # Обновляем изображение
@@ -136,7 +135,6 @@ class ImageGraphApp(QWidget):
         # Переключение
         self.image_index = (self.image_index + 1) % 2
 
-
     def open_video(self):
         try:
             # Open file dialog to select video
@@ -151,7 +149,6 @@ class ImageGraphApp(QWidget):
                 self.total_frames = int(self.video_cap.get(cv2.CAP_PROP_FRAME_COUNT))
                 self.max_frame = self.total_frames - 1
 
-
                 self.video_loaded = True
                 self.slider.setEnabled(True)
                 self.slider.setMinimum(0)
@@ -162,11 +159,62 @@ class ImageGraphApp(QWidget):
                 self.video_width = int(self.video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                 self.video_height = int(self.video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-
                 self.show_frame(0)
 
         except Exception as e:
             self.show_error_message(f"Error loading video: {e}")
+
+    def read_data(cond_dir, fname):
+        """Reads rotation angles and magnitudes, handling potential errors."""
+        try:
+            data = np.loadtxt(os.path.join(cond_dir, fname), delimiter=',', dtype=str)  # Assuming CSV with commas
+            magnitudes_fname = fname[:-10] + 'magnitudes.csv'  # Removing the _angles part
+            tbl = np.loadtxt(os.path.join(cond_dir, magnitudes_fname), delimiter=',',
+                             dtype=str)  # Assuming CSV with commas
+            return data, tbl
+        except (FileNotFoundError, ValueError) as e:
+            print(f"Error reading {e}")
+            return None, None
+
+    def load_csv(self):
+        try:
+            # Open file dialog to select CSV file
+            csv_file, _ = QFileDialog.getOpenFileName(
+                self, "Open CSV File", "", "CSV Files (*.csv)"
+            )
+            if csv_file:
+                # Load CSV with pandas
+                temp_list = []
+                for i in range(19, 39):
+                    if i % 3 == 0:
+                        continue
+                    temp_list.append(i)
+                temp_list.append(0)
+                temp_list.sort()
+                self.csv_data = pd.read_csv(csv_file, usecols=temp_list)
+
+                # Parse columns and identify pairs of x, y coordinates
+                self.coordinates = {}
+                columns = self.csv_data.columns
+
+                for i in range(1, len(columns), 2):  # Skip 'frame' column (0 index)
+                    if "x" in columns[i].lower() and "y" in columns[i + 1].lower():
+                        label = columns[i].lower().replace("_", "").replace("x", "").strip()
+                        self.coordinates[label] = (columns[i], columns[i + 1])
+
+                print("Detected Coordinates Pairs:", self.coordinates)
+        except Exception as e:
+            self.show_error_message(f"Error loading CSV: {e}")
+        data_init = np.loadtxt(os.path.join('Intact',
+                                            'Intact_1_angles.csv'),
+                               delimiter=',', dtype=str)
+        data = data_init[1:]
+        data = data.astype(np.float64)
+        column_data = data[0:, 2]
+        column_data = np.array(column_data)
+        column_data = column_data.astype(np.float64)
+
+        valid_data = column_data[~np.isnan(column_data)]  # Remove NaN values for calculation
 
     def show_frame(self, frame_number):
         try:
@@ -239,6 +287,16 @@ class ImageGraphApp(QWidget):
         except Exception as e:
             self.show_error_message(f"Error displaying frame: {e}")
 
+    def slider_changed_by_button(self):
+        position = self.slider.value() + 1
+        self.slider.setValue(position)
+        try:
+            # Display the frame corresponding to the slider's position
+            if self.video_loaded:
+                self.show_frame(position)
+        except Exception as e:
+            self.show_error_message(f"Error displaying frame: {e}")
+
     def keyPressEvent(self, event):
         """Handle key press events for the slider navigation."""
         if event.key() == Qt.Key_Right:  # Right arrow key
@@ -266,9 +324,9 @@ class ImageGraphApp(QWidget):
             self.video_cap.release()
         event.accept()
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = ImageGraphApp()
-    
     window.show()
     sys.exit(app.exec_())
